ef489c9706888cc56aa1b209812791fc578e7444
Sergio Fredes Mena <sergio.fredes@uniacc.edu>
2021-08-12 17:58:30 -0400

    Se elimina el sombreado al input del buscador - se incorpora el fondo de colecciones - se aplican condiciones para cada interfaz

--- themes/arde/javascripts/nfd.js
+++ themes/arde/javascripts/nfd.js
@@ -13,6 +13,35 @@ else if (document.location.pathname.indexOf("/search") > -1) {
   $(".logouno img").attr('src', '/omeka/themes/arde/images/arde-logo-color.png');
   $("header[role='banner']").css("height", "10px").css("background", "linear-gradient( rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8) ), url('/omeka/themes/arde/images/fondo-arde-v2-interior.jpg')").css({"maxHeight":"400px"}).css({"minHeight":"400px"});
 }
+else if (document.location.pathname.indexOf("/collections/browse") > -1) {
+  $(".logodos").css("display", "none");
+  $("#etiquetas").css("display", "none");
+  $("#destacado").css("display", "none");
+  $("#exposiciones").css("display", "none");
+  $("#colecciones").css("display", "none");
+  $(".mensaje-footer").css("display", "none");
+  $(".buscador").css("display", "none");
+  $(".nombre h1 a").css("border-bottom","3px solid white").css("padding-bottom","5px").text("Colecciones");
+  $(".logouno img").attr('src', '/omeka/themes/arde/images/arde-logo-color.png');
+  $("header[role='banner']").css("height", "10px").css("background", "linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6) ), url('/omeka/themes/arde/images/fondo-colecciones.jpg')").css({"maxHeight":"400px"}).css({"minHeight":"400px"});
+}
+else if (document.location.pathname.indexOf("/collections/show") > -1) {
+  $(".logodos").css("display", "none");
+  $("#etiquetas").css("display", "none");
+  $("#destacado").css("display", "none");
+  $("#exposiciones").css("display", "none");
+  $("#colecciones").css("display", "none");
+  $(".mensaje-footer").css("display", "none");
+}
+else if (document.location.pathname.indexOf("/items/show") > -1) {
+  $(".logodos").css("display", "none");
+  $("#etiquetas").css("display", "none");
+  $("#destacado").css("display", "none");
+  $("#exposiciones").css("display", "none");
+  $("#colecciones").css("display", "none");
+  $(".mensaje-footer").css("display", "none");
+  $("header[role='banner']").css("height", "10px").css("background", "#030303").css({"maxHeight":"150px"}).css({"minHeight":"150px"});
+}
 else {
 };
 
